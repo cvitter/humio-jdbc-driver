9baf06f34a83db5e2ddfc362271d76256b61f27a
Craig Vitter <craig.vitter@gmail.com>
2020-10-29 19:18:50 -0400

    First commit

---
+++ src/main/java/com/humio/jdbc/driver/Driver.java
@@ -0,0 +1,110 @@
+/** 
+ * Copyright (C) 2020 Craig Vitter
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.humio.jdbc.driver;
+
+import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class Driver implements java.sql.Driver {
+	// Variables need to be updated on a per release basis
+	private final static int MAJOR_VERSION = 0;
+	private final static int MINOR_VERSION = 1;
+	private final static boolean JDBC_COMPLIANT = false;
+	
+	/***
+	 * Register the driver with DriverManager
+	 */
+    static {
+        try {
+            DriverManager.registerDriver(new Driver());
+        } 
+        catch (SQLException e) {
+        }
+    }
+    
+    
+    /***
+     * Create connection to Humio using the server's url and the user
+     * token to authenticate againsts Humio's API
+     * @param info (url=String, token=String)
+     * @return Connection
+     * @throws SQLException
+     */
+	public Connection connect(Properties info) throws SQLException {
+		// Validate that either the URL is valid or required 
+		// information has been been passed via Properties (URL, Port)		
+		if (Utility.containsHumioProperties(info)) {
+			try {
+				return new com.humio.jdbc.driver.Connection(info);
+			} catch (UnknownHostException e) {
+				throw new SQLException(e);
+			}
+		}
+		else {
+			return null;
+		}
+	}
+	
+	@Override
+	/***
+	 * Humio requires both url and token, url alone will not work and will throw
+	 * an unknown host exception, pass via the info properties object
+	 * (url=String, token=String)
+     * @param url=String, info=Properties (url=String, token=String)
+     * @return Connection
+     * @throws SQLException
+	 */
+	public Connection connect(String url, Properties info) throws SQLException {
+		return connect(info);
+	}
+	
+
+
+	
+	
+	public int getMajorVersion() {
+		return MAJOR_VERSION;
+	}
+
+	public int getMinorVersion() {
+		return MINOR_VERSION;
+	}
+
+	public boolean jdbcCompliant() {
+		return JDBC_COMPLIANT;
+	}
+
+	
+	// Unsupported Driver methods
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+	
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		return false;
+	}
+	
+}
---
+++ src/main/java/com/humio/jdbc/driver/Utility.java
@@ -0,0 +1,171 @@
+/** 
+ * Copyright (C) 2020 Craig Vitter
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.humio.jdbc.driver;
+
+import java.util.Properties;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublishers;
+
+
+public class Utility {
+	
+	//static ResultSet _rs;
+	
+	
+	/***
+	 * queryHumio
+	 * @param url - Humio server url
+	 * @param token - User token used to authenticate to the Humio API
+	 * @param query - Query to run (SELECT, DELETE, HEALTH?)
+	 * @return
+	 * @throws InterruptedException 
+	 * @throws IOException 
+	 */
+	public static String queryHumio(String url, String token, String query) throws IOException, InterruptedException {
+		
+		// Determine the query type
+		String queryType = "UNKNOWN";
+		if (query.isBlank()) {
+			queryType = "STATUS";
+		}
+		else {
+			if (query.matches("/select/i")) queryType = "SELECT";
+			if (query.matches("/delete/i")) queryType = "DELETE";
+			if (query.matches("/health/i")) queryType = "HEALTH";
+		}
+		
+		// For select and delete events we need to know the repository
+		// And convert the SQL query to Humio's query format
+		String repository = "";
+		String messageBody = "";
+		if (queryType == "SELECT" || queryType == "DELETE") {
+			//repository = query.e
+		}
+		
+		// Build URL based on query type
+		String apiUrl = buildHumioUrl(url, queryType, repository);
+
+		
+		HttpResponse<String> response = queryHumioPost(apiUrl, token, messageBody);
+		return response.body();
+	}
+		
+	
+	// Humio API end points
+	private static String humioStatus = "api/v1/status";
+	private static String humioHealth = "api/v1/health";
+	private static String humioQueryBase = "api/v1/repositories/";
+	private static String humioSelect = "/query";
+	private static String humioDelete = "/deleteevents";
+	
+	/***
+	 * Build the correct URL to the Humio API based on the query type
+	 * @param url
+	 * @param queryType
+	 * @param repository
+	 * @return
+	 */
+	private static String buildHumioUrl(String url, String queryType, String repository) {
+		if (!url.endsWith("/")) url += "/";
+		switch(queryType) {
+			case "SELECT":
+				url += humioQueryBase + "/" + repository + "/" + humioSelect;
+				break;
+			case "DELETE":
+				url += humioQueryBase + "/" + repository + "/" + humioDelete;
+				break;
+			case "STATUS":
+				url += humioStatus;
+				break;
+			case "HEALTH":
+				url += humioHealth;
+				break;
+			case "UNKNOWN":
+				url = null;
+				break;
+		}
+		return url;
+	}
+	
+	
+	/***
+	 * httpClient - used in queryHumioPost below
+	 */
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+	
+	/***
+	 * queryHumioPost - Queries Humio's API via HTTP Post
+	 * @param url - API URL to post to
+	 * @param token - User API token to authenticate the query with
+	 * @param body - Body of the message to post
+	 * @return HttpResponse<String>
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	private static HttpResponse<String> queryHumioPost(String url, String token, String body) throws IOException, InterruptedException {
+		
+		
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(BodyPublishers.ofString(body))
+                .uri(URI.create(url))
+                .setHeader("Authorization", "Bearer " + token)
+                .setHeader("Content-Type", "application/json")
+                .build();
+        
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        return response;
+	}
+	
+	
+	
+	
+	/***
+	 * Checks to see if url and token exist in info
+	 * @param info Properties object with url and token keys
+	 * @return True or False
+	 */
+	// TODO: Make this check more meaningful
+	public static boolean containsHumioProperties(Properties info) {
+		// Make sure info isn't empty or null
+		if (info.isEmpty() != true && info != null) {
+			
+			if (info.getProperty("url").isEmpty() != true && info.getProperty("url").isBlank() != true &&
+				isValidUrl(info.getProperty("url")) && 
+				info.getProperty("token").isEmpty() != true && info.getProperty("token").isBlank() != true) {
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
+	/***
+	 * Checks to see if the url string appears to be valid
+	 * @param url
+	 * @return
+	 */
+	// TODO: Make this check more meaningful
+	public static boolean isValidUrl(String url) {
+		if (url.startsWith("http://") || url.startsWith("https://")) return true;
+		return false;
+	}
+
+
+}
