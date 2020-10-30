3e96e9be2493635dd21e06c7edaa34b2c34800ab
Craig Vitter <craig.vitter@gmail.com>
2020-10-30 10:50:28 -0400

    Add properties
    Add properties file and implement use in UtilityTest

--- src/test/java/com/humio/jdbc/driver/UtilityTest.java
+++ src/test/java/com/humio/jdbc/driver/UtilityTest.java
@@ -3,6 +3,8 @@ package com.humio.jdbc.driver;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,6 +13,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class UtilityTest {
+	
+	private String humioUrl = "";
+	private String apiToken = "";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -22,6 +27,26 @@ public class UtilityTest {
 
 	@Before
 	public void setUp() throws Exception {
+		// Read in test.properties file
+        try (InputStream input = UtilityTest.class.getClassLoader().getResourceAsStream("test.properties")) {
+
+            Properties prop = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            //get the property value and print it out
+            humioUrl = prop.getProperty("humiourl");
+            apiToken = prop.getProperty("apitoken");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 	}
 
 	@After
@@ -39,8 +64,8 @@ public class UtilityTest {
 	
 	@Test
 	public void testHumioStatusQuery() throws IOException, InterruptedException {
-		String response = com.humio.jdbc.driver.Utility.queryHumio("", "", "");
-		
+		String response = com.humio.jdbc.driver.Utility.queryHumio(humioUrl, apiToken, "");
+		System.out.print(response);		
 	}
 
 }
